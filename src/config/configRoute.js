922e376d150840727d497d646a72aa811f540fbb
Darshan Gautam <dsrsangautam@gmail.com>
2022-06-15 16:43:37 +0545

    created the product api

--- src/config/configRoute.js
+++ src/config/configRoute.js
@@ -2,5 +2,5 @@ export const BASE_URL = 'http://192.168.1.74:3001/api/v1/';
 
 export const apiRoutes = {
   getProducts: 'product/',
-  getAllCategories: 'category',
+  getCategories: 'category/',
 };
--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,18 @@
 import {configureStore} from '@reduxjs/toolkit';
 import {setupListeners} from '@reduxjs/toolkit/query';
 import {productApi} from '../services/productApi';
+import {categoryApi} from '../services/categoryApi';
 
 export const store = configureStore({
   reducer: {
     [productApi.reducerPath]: productApi.reducer,
+    [categoryApi.reducerPath]: categoryApi.reducer,
   },
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(productApi.middleware),
+    getDefaultMiddleware().concat(
+      productApi.middleware,
+      categoryApi.middleware,
+    ),
 });
 
 setupListeners(store.dispatch);

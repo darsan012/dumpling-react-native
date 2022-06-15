efb0b5c8feb1ab532363f5a26334bfee709a0d99
Darshan Gautam <dsrsangautam@gmail.com>
2022-06-15 12:35:38 +0545

    setup of the store and added product api

--- App.js
+++ App.js
@@ -9,6 +9,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Provider} from 'react-redux';
 
 import screens from './src/routes/routes';
 

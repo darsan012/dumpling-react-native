84bdfcf53ff3d754be89bb4bd6413b087f681261
Darshan Gautam <dsrsangautam@gmail.com>
2022-06-15 22:53:30 +0545

    added the product slice on the store

--- src/store/store.js
+++ src/store/store.js
@@ -2,9 +2,11 @@ import {configureStore} from '@reduxjs/toolkit';
 import {setupListeners} from '@reduxjs/toolkit/query';
 import {productApi} from '../services/productApi';
 import {categoryApi} from '../services/categoryApi';
+import productReducer from '../store/slices/productSlice';
 
 export const store = configureStore({
   reducer: {
+    productDetails: productReducer,
     [productApi.reducerPath]: productApi.reducer,
     [categoryApi.reducerPath]: categoryApi.reducer,
   },

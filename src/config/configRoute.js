b5dc045fba0cb3c55f31cbdc0622bb210cc884fe
Sabita Sitaula <sabitasitaula705@gmail.com>
2022-06-16 17:01:49 +0545

    implemented searching product in search screen

--- src/config/configRoute.js
+++ src/config/configRoute.js
@@ -1,6 +1,7 @@
-export const BASE_URL = 'http://192.168.1.74:3001/api/v1/';
+export const BASE_URL = 'http://192.168.1.83:3001/api/v1/';
 
 export const apiRoutes = {
   getProducts: 'product/',
   getCategories: 'category/',
+  getFeaturedProducts:'product/get/featured'
 };
--- src/screen/Search/SearchScreen.js
+++ src/screen/Search/SearchScreen.js
@@ -1,46 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ScrollView, Text, StyleSheet, View} from 'react-native';
-import Header from '../components/Headers';
-import IconComponent from '../components/IconComponent';
-import Input from '../components/Input';
+import Header from '../../components/Headers';
+import IconComponent from '../../components/IconComponent';
+import Input from '../../components/Input';
 import {faFilter} from '@fortawesome/free-solid-svg-icons';
-import FeaturedCard from '../components/FeaturedCard';
-import chilly from '../assets/chillyMomo.jpg';
+import FeaturedCard from '../../components/FeaturedCard';
+import chilly from '../../assets/chillyMomo.jpg';
+import {useGetAllProductsQuery} from '../../services/productApi';
+
+const filterProduct = (productData, query) => {
+  console.log(productData)
+  if (query.trim() === '') {
+    return productData;
+  }
+  console.log(query);
+  return productData.filter(data =>
+  {
+    const productName = data.name.toLowerCase();
+    return productName.includes(query);
+  }
+  );
+};
 
 const SearchScreen = () => {
+  const {data, isLoading, error} = useGetAllProductsQuery();
+  const [query, setQuery] = useState('');
+  let filteredItems = [];
+  data && (filteredItems = filterProduct(data.data, query));
   return (
     <ScrollView>
-      <Header text="Find your favorite MOMO" fontSize={20} />
+      <Header text="Find your favorite MOMO" fontSize={25} />
       <View style={styles.searchWrapper}>
         <View style={styles.searchBar}>
-          <Input placeholder="Search by name, category..." />
+          <Input
+            placeholder="Search by name, category..."
+            searchQueryHandler={setQuery}
+            value={query}
+          />
         </View>
         <IconComponent iconName={faFilter} size={25} />
       </View>
-      <FeaturedCard
-        momoImage={chilly}
-        momoName="Chilli MOMO"
-        momoPrice={200}
-        momoDescription="Momo made with proper sanitation"
-      />
-      <FeaturedCard
-        momoImage={chilly}
-        momoName="Chilli MOMO"
-        momoPrice={200}
-        momoDescription="Momo made with proper sanitation"
-      />
-      <FeaturedCard
-        momoImage={chilly}
-        momoName="Chilli MOMO"
-        momoPrice={200}
-        momoDescription="Momo made with proper sanitation"
-      />
-      <FeaturedCard
-        momoImage={chilly}
-        momoName="Chilli MOMO"
-        momoPrice={200}
-        momoDescription="Momo made with proper sanitation"
-      />
+      {isLoading && <Text style={{color: 'black'}}>Loading....</Text>}
+      {error && <Text style={{color: 'black'}}>Error....</Text>}
+      {data &&
+        filterProduct(data.data, query).map(obj => (
+          <FeaturedCard
+            key={obj.productId}
+            momoImage={chilly}
+            momoName={obj.name}
+            momoPrice={obj.price}
+            momoDescription={obj.description}
+          />
+        ))}
     </ScrollView>
   );
 };

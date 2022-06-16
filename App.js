1a39154dd83554cf4613b32fd8bd996558a20b5f
Sabita Sitaula <sabitasitaula705@gmail.com>
2022-06-16 10:40:50 +0545

    Created layout of dumpling app

--- App.js
+++ App.js
@@ -1,38 +1,50 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Provider} from 'react-redux';
 import {store} from './src/store/store';
+import IconComponent from './src/components/IconComponent';
 
 import screens from './src/routes/routes';
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Stack.Navigator>
+        <Tab.Navigator>
           {screens.map(obj => (
-            <Stack.Screen
+            <Tab.Screen
               key={obj.name}
               name={obj.name}
               component={obj.component}
-              options={{headerShown: obj.showHeader}}
+              options={() => ({
+                tabBarIcon: ({focused, color}) => {
+                  let iconSize;
+
+                  iconSize = focused ? 30 : 27;
+
+                  return (
+                    <IconComponent
+                      iconName={obj.icon}
+                      color={color}
+                      size={iconSize}
+                    />
+                  );
+                },
+                tabBarActiveTintColor: 'tomato',
+                tabBarInactiveTintColor: 'gray',
+                headerShown: obj.showHeader,
+                tabBarLabelStyle: {fontSize: 15, fontWeight: 'bold'},
+              })}
             />
           ))}
-        </Stack.Navigator>
+        </Tab.Navigator>
       </NavigationContainer>
     </Provider>
   );
 };
 
 export default App;
+

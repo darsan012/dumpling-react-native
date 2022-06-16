992ccc7faf5e25e5d76bb23c79b959a0c6ddd1ad
Sabita Sitaula <sabitasitaula705@gmail.com>
2022-06-16 11:44:50 +0545

    Created Hero Section

---
+++ src/constants/Constants.js
@@ -0,0 +1,31 @@
+export const stylesConstant = {
+    color: {
+      primaryColor: '#000',
+      secondaryColor: 'white',
+      borderRadius: '10',
+      borderWidth: 5,
+      colorDanger: 'red',
+      colorWarning: 'orange',
+      colorSuccess: 'green',
+      colorDefault: '#003532',
+      inActiveColor: 'grey',
+      backgroundColor: '#F6F5F8',
+      cardBackgroundColor: '#FFF',
+      btnColor: '#FF4500',
+      inActiveColor: 'grey',
+    },
+    size: {
+      productCardWidth: 180,
+      productCardHeight: 170,
+      productCardFintSize: 5,
+      btnTextSize: 12,
+    },
+    textSize: {
+      headers: 17,
+    },
+  };
+  
+  
+  
+  
+  
--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -1,7 +1,19 @@
 import React, {useEffect} from 'react';
-import {View, Text, SafeAreaView, ScrollView} from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  Image,
+  StyleSheet,
+  ImageBackground,
+} from 'react-native';
 import {useSelector, useDispatch} from 'react-redux';
 import {useLazyGetAllProductsQuery} from '../services/productApi';
+import logo from '../assets/logo.png';
+import hero from '../assets/hero.jpg';
+
+import {stylesConstant} from '../constants/Constants';
 
 const HomeScreen = () => {
   const [getAllProducts, response] = useLazyGetAllProductsQuery();
@@ -10,11 +22,34 @@ const HomeScreen = () => {
     <SafeAreaView>
       <ScrollView>
         <View>
-          <Text>This is home screen</Text>
+          <ImageBackground source={hero} style={styles.hero}>
+            <View style={styles.logoView}>
+              <Image source={logo} style={styles.logo} />
+            </View>
+          </ImageBackground>
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 50,
+    height: 50,
+  },
+  hero: {
+    height: 300,
+    width: '100%',
+  },
+  logoView: {
+    backgroundColor: 'white',
+    borderRadius: 100,
+    width: 55,
+    height: 55,
+    justifyContent: 'center',
+    alignItems:'center'
+  }
+});
 
 export default HomeScreen;
